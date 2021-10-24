924736c8d62cf53f1e65391454685dd5df2fd65b
Deboramc05 <92049868+Deboramc05@users.noreply.github.com>
2021-10-24 19:36:31 -0300

    direcionamento javascript para index.html

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+function enviado() {
+    let nome = document.getElementById('nome').value;
+    alert(` ${nome}, Muito obrigado(a)! Recebemos sua mensagem e assim que possivel entramos em contato.`)
+}
